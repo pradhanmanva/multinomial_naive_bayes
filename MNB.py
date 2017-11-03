602fe2922170720617e5ccc496e71e37333ba3c4
Manva Pradhan <pradhanmanva@gmail.com>
2017-11-02 20:55:01 -0500

    Improved the accuracy from 79% to 81%

--- MNB.py
+++ MNB.py
@@ -10,8 +10,20 @@ import numpy as np
 from nltk.corpus import stopwords
 
 
+def pre_process_data(path, name, list):
+    f = open(os.path.join(path, name, list))
+    read_file = str(f.readlines())
+    read_file = re.sub(r'[<|>|?|_|,|!|:|;|(|)|\"|=|-|$|\\|/|*|\'|+|\[|\]|#|$|%|^|?|~|`]', r'',
+                       read_file)
+    tokens = nltk.word_tokenize(read_file)
+    stop_list = stopwords.words('english')
+    read_file = [word.lower() for word in tokens if
+                 word.isalpha() and not word in stop_list and len(word) > 2]
+    return read_file
+
+
 def training_mnb(class_list, training_set):
-    #count = 1
+    # count = 1
     vocabulary = list()
     total_docs = 0
     count_docs_in_class = list()
@@ -24,19 +36,13 @@ def training_mnb(class_list, training_set):
             for file_list in os.listdir(os.path.join(training_set, name)):
                 total_docs += 1
                 local_docs += 1
-                f = open(os.path.join(training_set, name, file_list))
-                read_file_class = str(f.readlines())
-                tokens = nltk.word_tokenize(read_file_class)
-                stop_list = stopwords.words('english')
-                read_file_class = str([word.lower() for word in tokens if
-                                       word.isalpha() and not word in stop_list and len(word) > 2])
-                read_file_class = re.sub(r'[<|>|?|_|,|!|:|;|(|)|\"|=|-|$|\\|/|*|\'|+|\[|\]|#|$|%|^|?|~|`]', r'',
-                                         read_file_class)
+                word_list = pre_process_data(training_set, name, file_list)
+                read_file_class = " ".join(word_list)
                 test_class = test_class + " " + read_file_class
             vocabulary.extend(test_class.split())
             text_of_each_class.append(test_class)
             count_docs_in_class.append(local_docs)
-            #count += 1
+            # count += 1
     vocabulary = set(vocabulary)
     i = 0
     conditional_prob_class = list()
@@ -61,20 +67,14 @@ def training_mnb(class_list, training_set):
 
 def apply_mnb(class_list, prior, conditional_prob_class, document, test_set):
     score = list()
-    f = open(os.path.join(test_set, name, document))
-    token_string = " ".join(f.readlines())
-    token_list = re.sub(r'[<|>|?|_|,|!|:|;|(|)|\"|=|-|$|\\|/|*|\'|+|\[|\]|#|$|%|^|?|~|`]', r'', token_string)
-    tokens = nltk.word_tokenize(token_list)
-    stop_list = stopwords.words('english')
-    w_vocabulary = set([word.lower() for word in tokens if word.isalpha() and not word in stop_list and len(word) > 2])
-    m = 0
-    for class_name in class_list:
+    word_list = pre_process_data(test_set, name, document)
+    w_vocabulary = set(word_list)
+    for m in range(len(class_list)):
         score.append(math.log(prior[m]))
         for term in w_vocabulary:
             if not conditional_prob_class[m][term]:
                 continue
             score[m] += math.log(conditional_prob_class[m][term][0])
-        m += 1
     # score = np.array(score)
     return np.argmax(np.array(score))
 
@@ -86,26 +86,23 @@ if __name__ == "__main__":
     class_list = list()
 
     dir_name = os.path.split(training_set)[1]
-    #count = 1
-    #folders = 5
     for name in sorted(os.listdir(training_set)):
-        if os.path.isdir(os.path.join(training_set, name)) and '''count <= folders''':
+        if os.path.isdir(os.path.join(training_set, name)):
             class_list.append(name)
-            #count += 1
 
-    #count = 1
     vocabulary, prior_list, conditional_prob_class = training_mnb(class_list, training_set)
     success = 0
     failure = 0
     for name in sorted(os.listdir(test_set)):
-        if os.path.isdir(os.path.join(test_set, name)) and '''count <= folders''':
+        if os.path.isdir(os.path.join(test_set, name)):
             for doc in os.listdir(os.path.join(test_set, name)):
                 folder_predicted = apply_mnb(class_list, prior_list, conditional_prob_class, doc, test_set)
                 if class_list[int(folder_predicted)] == name:
                     success += 1
                 else:
                     failure += 1
-        #count += 1
+        # count += 1
     print("Success : " + str(success))
     print("Failure : " + str(failure))
+    print("Total : " + str(success + failure))
     print("Accuracy : " + str(success * 100 / (success + failure)))
